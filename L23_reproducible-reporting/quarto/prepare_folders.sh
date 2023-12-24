182a2221b4f0b9a0572613c4979036619c229c32
Francesco L <francesco.lescai@unipv.it>
2023-12-24 13:04:40 +0100

    adding code for quarto report

---
+++ L23_reproducible-reporting/quarto/prepare_folders.sh
@@ -0,0 +1,7 @@
+mkdir -p /home/rstudio/dati_rstudio/reporting
+cd /home/rstudio/dati_rstudio/reporting
+
+## data are assumed to be in
+## /home/rstudio/dati_vscode/variant_calling/variants
+## and 
+## /home/rstudio/dati_vscode/datasets_reference_only/annotations
