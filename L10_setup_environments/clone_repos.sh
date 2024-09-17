f097a367b5226beed7b573c9591a9cbcbb27876d
Francesco L <francesco.lescai@unipv.it>
2024-09-17 17:30:01 +0200

    added clone repos

---
+++ L10_setup_environments/clone_repos.sh
@@ -0,0 +1,22 @@
+
+###########################################
+## for the environment of variant calling #
+###########################################
+
+
+cd /workspaces/class-variantcalling
+
+git clone https://github.com/lescai-teaching/class-bsa.git
+git clone https://github.com/lescai-teaching/datasets-class-variantcalling.git
+git clone https://github.com/lescai-teaching/datasets_reference_only.git
+
+
+###########################################
+## for the environment of RNA sequencing  #
+###########################################
+
+cd /workspaces/class-rnaseq
+
+git clone https://github.com/lescai-teaching/class-bsa.git
+git clone https://github.com/lescai-teaching/datasets-class-rnaseq.git
+git clone https://github.com/lescai-teaching/datasets_reference_only.git
